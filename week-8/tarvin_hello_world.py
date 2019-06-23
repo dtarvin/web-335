b4db8a63958444a5a6b730d2e90bade3101cceb1
David R Tarvin Jr <david.tarvin@gmail.com>
2019-06-23 10:47:25 -0500

    add exercise 8.2

---
+++ week-8/tarvin_hello_world.py
@@ -0,0 +1,10 @@
+#============================================
+# Title:  tarvin_hello_world.py
+# Author: Richard Krasso
+# Modified by: David Tarvin
+# Date:   21 June 2019
+# Description: Exercise 8.2
+#===========================================
+
+print('Hello World')
+
