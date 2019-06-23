fda8cad777e8437f59282381d271f784a5b5aa60
David R Tarvin Jr <david.tarvin@gmail.com>
2019-06-23 10:59:12 -0500

    add exercise 8.3

---
+++ week-8/tarvin_calculator.py
@@ -0,0 +1,21 @@
+#============================================
+# Title:  tarvin_calculator.py
+# Author: Richard Krasso
+# Modified by: David Tarvin
+# Date:   21 June 2019
+# Description: Exercise 8.3
+#===========================================
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1/num2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
+
